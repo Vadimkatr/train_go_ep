92e0c7c27ebd4b7a0d5a01cd0e2c719e81f6eb74
Vadimkatr <vadimlisai@gmail.com>
2019-11-29 15:44:10 +0300

    impl task5.1, task5.2, task5.3

--- task5/converter/converter.go
+++ task5/converter/converter.go
@@ -1,5 +1,19 @@
 package converter
 
-func MyStrToInt(s string) int {
-	return 0
+import (
+	"fmt"
+	"strconv"
+)
+
+func MyStrToIntUseAtoi(s string) (int, error) {
+	return strconv.Atoi(s)
+}
+
+func MyStrToIntUseFmt(s string) (int, error) {
+	var x int
+	n, err := fmt.Sscanf(s, "%d\r", &x);
+	if n != 1 || err != nil {
+		return 0, err
+	  }
+	return x, nil
 }
--- task5/main.go
+++ task5/main.go
@@ -4,8 +4,33 @@ import (
 	"fmt"
 
 	"train_go_ep/task5/converter"
+	"train_go_ep/task5/thirdtask"
 )
 
 func main() {
-	fmt.Printf("%d", converter.MyStrToInt("0"))
+	// task 1: implement myStrToInt func with tests and coverage;
+	// see func converter.MyStrToIntUseAtoi,
+	// converter/converter_test.go for tests;
+	// and converter/coverout.html for coverage;
+	text := "123456789"
+	n, err := converter.MyStrToIntUseAtoi(text)
+	if err != nil {
+		fmt.Printf("MyStrToIntUseAtoi Error %v\n", err)
+	} else {
+		fmt.Printf("MyStrToIntUseAtoi Number %d\n", n)
+	}
+
+	// task 2: implement another myStrToInt func that pass tests of first;
+	// see func converter.MyStrToIntUseFmt,
+	// converter/converter_test.go for tests;
+	// and converter/tests_results.txt for benchmarks;
+	n, err = converter.MyStrToIntUseFmt(text)
+	if err != nil {
+		fmt.Printf("MyStrToIntUseFmt Error %v\n", err)
+	} else {
+		fmt.Printf("MyStrToIntUseFmt Number %d\n", n)
+	}
+
+	// task 3: fix error in code
+	thirdtask.MainOfTherdTask()
 }
